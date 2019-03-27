ea25f4ec9e11e44d7ec9fb98ebb72aa9d8602810
“Polixenia <polixeniacorreia@gmail.com>
2019-03-27 16:33:00 -0300

    Mudança de Atualização

--- src/Visitas.js
+++ src/Visitas.js
@@ -5,9 +5,9 @@ const Visitas = ({ navigation }) => (
   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     <Text>Visitas Fiscal Native ;D</Text>
     <Button 
-      title="Abrir Empresa"
+      title="ADCIONAR"
       onPress={() => navigation.navigate('Estabelecimento') }
-    />
+    /> 
   </View>
 );
 
