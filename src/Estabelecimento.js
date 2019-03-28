735b9f45c5bff3e1eb51e1acf60372e013c34685
Emiliano Carvalho <emilianocarvalho.dev@gmail.com>
2019-03-27 22:08:47 -0300

    Nomes dos componentes

--- src/Estabelecimento.js
+++ src/Estabelecimento.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { View, Button, Text } from "react-native";
 
-const Page2 = () => (
+const Estabelecimento = () => (
   <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
     <Text>Estabelecimento</Text>
   </View>
 );
 
-Page2.navigationOptions = {
+Estabelecimento.navigationOptions = {
   title: "Estabelecimento"
 };
 
-export default Page2;
+export default Estabelecimento;
--- src/Visitas.js
+++ src/Visitas.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { View, Button, Text } from 'react-native';
+import React from "react";
+import { View, Button, Text } from "react-native";
 
 const Visitas = ({ navigation }) => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Visitas Fiscal Native ;D</Text>
-    <Button 
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text>Hello, World!</Text>
+    <Text>Visitas Fiscal Native</Text>
+    <Button
       title="Abrir Empresa"
-      onPress={() => navigation.navigate('Estabelecimento') }
+      onPress={() => navigation.navigate("Estabelecimento")}
     />
   </View>
 );
 
 Visitas.navigationOptions = {
-  title: 'Visitas',
-}
+  title: "Visitas"
+};
 
 export default Visitas;
